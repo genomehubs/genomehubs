1eafcce6f2277c797804def9fe361642354cdbe3
Richard Challis <rjchallis@gmail.com>
2021-03-01 14:01:39 +0000

    'Bump version: 2.0.1 → 2.0.2'

--- scripts/conda_build.sh
+++ scripts/conda_build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION=2.0.1
+VERSION=2.0.2
 
 case $(uname | tr '[:upper:]' '[:lower:]') in
   linux*)
--- src/genomehubs/lib/version.py
+++ src/genomehubs/lib/version.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 """genomehubs version."""
 
-__version__ = "2.0.1"
+__version__ = "2.0.2"
