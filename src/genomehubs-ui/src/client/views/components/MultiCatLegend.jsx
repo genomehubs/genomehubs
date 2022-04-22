36b5de6254865dc98384ceaab87aeaa61ab2c9b7
Richard Challis <richard.challis@sanger.ac.uk>
2022-04-22 15:42:33 +0100

    use new legend for rainbow plot

--- src/genomehubs-ui/src/client/views/components/MultiCatLegend.jsx
+++ src/genomehubs-ui/src/client/views/components/MultiCatLegend.jsx
@@ -3,11 +3,18 @@ import { Rectangle, Text } from "recharts";
 import React from "react";
 import formats from "../functions/formats";
 
-const MultiCatLegend = ({ width, x, fill, i, n = 1, name, stats }) => {
+const MultiCatLegend = ({
+  width,
+  x,
+  fill,
+  i,
+  n = 1,
+  name,
+  stats,
+  legendWidth = 150,
+}) => {
   let cellSize = 15;
-  let legendWidth = 150;
   let row = Math.floor((width - 100) / legendWidth);
-
   let j = 1;
   if (i > row) {
     while (j * (row + 1) < i + 1) {
@@ -23,6 +30,48 @@ const MultiCatLegend = ({ width, x, fill, i, n = 1, name, stats }) => {
     i += j * (row + 1) - n;
   }
   let xPos = x + width - legendWidth * (row - i);
+
+  let value;
+  if (stats) {
+    console.log(stats);
+    if (stats.sum) {
+      value = (
+        <Text
+          x={-5}
+          y={cellSize}
+          fill={"rgb(102, 102, 102)"}
+          dominantBaseline={"central"}
+          textAnchor={"end"}
+        >
+          {`n=${formats(stats.sum, "integer")}${
+            stats.sum > 0 && stats.max > stats.min
+              ? ` [${formats(stats.min, "integer")}-${formats(
+                  stats.max,
+                  "integer"
+                )}]`
+              : ""
+          }`}
+        </Text>
+      );
+    } else if (stats.count) {
+      console.log(stats.count);
+      value = (
+        <text
+          x={-5}
+          y={cellSize}
+          fill={"rgb(102, 102, 102)"}
+          dominantBaseline={"central"}
+          textAnchor={"end"}
+        >
+          {/* <tspan>{"n="}</tspan> */}
+          <tspan fill={fill}>{formats(stats.count, "integer")}</tspan>
+          {stats.total && (
+            <tspan>{` / ${formats(stats.total, "integer")}`}</tspan>
+          )}
+        </text>
+      );
+    }
+  }
 
   let text = (
     <g
@@ -41,31 +90,14 @@ const MultiCatLegend = ({ width, x, fill, i, n = 1, name, stats }) => {
       >
         {name}
       </Text>
-      {stats && (
-        <Text
-          x={-5}
-          y={cellSize}
-          fill={"rgb(102, 102, 102)"}
-          dominantBaseline={"central"}
-          textAnchor={"end"}
-        >
-          {`n=${formats(stats.sum, "integer")}${
-            stats.sum > 0 && stats.max > stats.min
-              ? ` [${formats(stats.min, "integer")}-${formats(
-                  stats.max,
-                  "integer"
-                )}]`
-              : ""
-          }`}
-        </Text>
-      )}
+      {value}
       <Rectangle
         key={`cell-${i}`}
         height={cellSize * 2}
         width={cellSize / 2}
         fill={fill || "rgb(102, 102, 102)"}
         x={0} // {props.cx + (w - width) / 2}
-        y={-cellSize / 2}
+        y={-cellSize * 0.75}
         style={{ pointerEvents: "none" }}
       />
     </g>
