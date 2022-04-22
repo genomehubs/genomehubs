c1e9aebd5602be4c90a1c95d33870e9f21d6f4fc
Richard Challis <richard.challis@sanger.ac.uk>
2022-04-22 16:00:50 +0100

    remove debugging message

--- src/genomehubs-ui/src/client/views/components/ReportXInY.jsx
+++ src/genomehubs-ui/src/client/views/components/ReportXInY.jsx
@@ -138,7 +138,6 @@ const PieComponent = ({ data, height, width, colors }) => {
 
 const RadialBarComponent = ({ data, height, width, colors }) => {
   const renderRadialBarLabel = (props) => {
-    console.log(props);
     const { cx, cy, index, viewBox, fill, value, data, background, width, n } =
       props;
     const fontSize = (viewBox.outerRadius - viewBox.innerRadius) / 2;
