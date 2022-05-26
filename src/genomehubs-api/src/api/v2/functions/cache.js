0b6b751300b3252307a39feabfc99d3d151e4b10
Richard Challis <richard.challis@sanger.ac.uk>
2022-05-26 16:22:58 +0100

    remove queryId from url before using as cache key

--- src/genomehubs-api/src/api/v2/functions/cache.js
+++ src/genomehubs-api/src/api/v2/functions/cache.js
@@ -1,8 +1,18 @@
 import { mc } from "./memcached";
+import qs from "qs";
+
+const sortUrl = (url) => {
+  let [path, search] = url.split(/[\?#]/);
+  let { queryId, ...options } = qs.parse(search);
+  let sortedOptions = Object.entries(options || {})
+    .sort((a, b) => a[0].localeCompare(b[0]))
+    .reduce((r, [key, value]) => ({ ...r, [key]: value }), {});
+  return `${path}?${qs.stringify(sortedOptions)}`;
+};
 
 export const cacheFetch = async (req) => {
   if (mc) {
-    const key = req.url;
+    const key = sortUrl(req.url);
     try {
       let cachedData = await mc.get(key);
       return JSON.parse(cachedData);
@@ -17,7 +27,7 @@ export const cacheFetch = async (req) => {
 export const cacheStore = async (req, obj) => {
   if (mc) {
     try {
-      const key = req.url;
+      const key = sortUrl(req.url);
       const value = JSON.stringify(obj);
       if (value.length <= 1024 * 1024 * 32) {
         mc.set(key, JSON.stringify(obj));
