ca281dd21085aaf599c058b4bac799bd166c698f
Richard Challis <richard.challis@sanger.ac.uk>
2022-06-06 11:34:14 +0100

    exit UI tests with pytest return code

--- run_ui_tests.py
+++ run_ui_tests.py
@@ -1,7 +1,11 @@
+"""Run UI tests using selenium and pytest."""
+
 import os
 import pathlib
+import sys
 
 import pytest
 
 os.chdir(pathlib.Path.cwd() / "tests" / "integration_tests" / "ui")
-pytest.main()
+exit_code = pytest.main()
+sys.exit(exit_code)
