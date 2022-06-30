3251e1aa6ab163d4de1c776d4cbe9e4c51578259
Richard Challis <richard.challis@sanger.ac.uk>
2022-06-30 16:08:45 +0100

    "Bump version: 2.4.22 → 2.4.23"

--- scripts/conda_build.sh
+++ scripts/conda_build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION=2.4.22
+VERSION=2.4.23
 
 case $(uname | tr '[:upper:]' '[:lower:]') in
   linux*)
--- src/genomehubs/lib/version.py
+++ src/genomehubs/lib/version.py
@@ -1,4 +1,4 @@
 #!/usr/bin/env python3
 """genomehubs version."""
 
-__version__ = "2.4.22"
+__version__ = "2.4.23"
