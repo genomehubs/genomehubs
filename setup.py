f832df39c0a0f3388442fa36551d45a5fd6ec7f9
Richard Challis <richard.challis@sanger.ac.uk>
2022-07-08 16:22:01 +0100

    "Bump version: 2.4.25 → 2.4.26"

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*names, **kwargs):
 
 setup(
     name="genomehubs",  # Required
-    version="2.4.25",
+    version="2.4.26",
     description="GenomeHubs",  # Optional
     long_description="%s\n%s"
     % (
