ab3619ce17e1617423f2728132045eb8d62a8d03
Richard Challis <richard.challis@sanger.ac.uk>
2022-11-04 13:54:11 +0000

    fix ui histogram test

--- tests/integration_tests/ui/test_landing.py
+++ tests/integration_tests/ui/test_landing.py
@@ -1,5 +1,7 @@
 """Landing page tests."""
 
+
+import contextlib
 import pytest
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -19,26 +21,22 @@ class TestLandingPage:
 
     def test_histogram(self):
         """Check histogram loads."""
-        self.driver.get(f"{BASE_URL}/report?report=histogram&x=assembly_date&rank=species&cat=assembly_level&stacked=true&includeEstimates=true&excludeAncestral%5B0%5D=assembly_span&excludeMissing%5B0%5D=assembly_span&caption=Progress%20of%20genome%20assemblies%20published%20on%20INSDC%20over%20time%2C%20by%20assembly%20level&taxonomy=ncbi&result=taxon")
+        self.driver.get(
+            f"{BASE_URL}/report?report=histogram&x=assembly_date&rank=species&cat=assembly_level&stacked=true&includeEstimates=true&excludeAncestral%5B0%5D=assembly_span&excludeMissing%5B0%5D=assembly_span&caption=Progress%20of%20genome%20assemblies%20published%20on%20INSDC%20over%20time%2C%20by%20assembly%20level&taxonomy=ncbi&result=taxon"
+        )
         histogram = None
-        try:
+        with contextlib.suppress(Exception):
             WebDriverWait(self.driver, 10).until(
-                EC.presence_of_element_located((By.ID, "histogram"))
+                EC.visibility_of_element_located((By.ID, "histogram"))
             )
             histogram = self.driver.find_element(By.ID, "histogram")
-        except Exception:
-            pass
         assert histogram is not None
         svg = None
-        try:
+        with contextlib.suppress(Exception):
             svg = histogram.find_element(By.TAG_NAME, "svg")
-        except Exception:
-            pass
         assert svg is not None
         bars = None
-        try:
+        with contextlib.suppress(Exception):
             bars = svg.find_elements(By.CLASS_NAME, "recharts-bar")
-        except Exception:
-            pass
         assert bars is not None
         assert len(bars) == 6
