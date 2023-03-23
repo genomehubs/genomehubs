0a4aed9723e181dce7e0674468799b4517b734a6
Richard Challis <richard.challis@sanger.ac.uk>
2023-03-23 09:42:36 +0000

    support elasticsearch 8.6.2 in command line

--- setup.py
+++ setup.py
@@ -99,7 +99,7 @@ setup(
     install_requires=[
         "biopython>=1.78",
         "docopt>=0.6.2",
-        "elasticsearch>=7.8.1,<7.14.0",
+        "elasticsearch==8.6.2",
         "fastjsonschema>=2.15.3",
         "filetype>=1.0.7",
         "h3>=3.7.4",
