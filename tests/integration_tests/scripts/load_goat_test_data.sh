7e6e36612957a32df9cccc88a6ad191cfc912926
Richard Challis <richard.challis@sanger.ac.uk>
2023-05-30 11:37:51 +0100

    Bugfix/run-on-self-hosted (#276)

--- tests/integration_tests/scripts/load_goat_test_data.sh
+++ tests/integration_tests/scripts/load_goat_test_data.sh
@@ -77,10 +77,19 @@ genomehubs index \
     --config-file tests/integration_tests/config/goat.yaml \
     --taxonomy-source ncbi \
     --file-metadata tests/integration_tests/data/btk/btk.files.yaml &&
+if [ ! -z "$CONTAINER_VERSION" ]; then
+  GH_RELEASE=2021.10.15 GH_HUBNAME=goat GH_API_PORT=3001 /genomehubs/genomehubs-api &
+  API_PID=$!
+else 
+  GH_RELEASE=2021.10.15 GH_HUBNAME=goat GH_API_PORT=3001 ./dist/genomehubs-api &
+  API_PID=$!
+fi
+sleep 5 &&
 genomehubs test \
     --config-file tests/integration_tests/config/goat.yaml \
-    --base-url http://localhost:3000/api/v2 \
+    --base-url http://localhost:3001/api/v2 \
     --json-test-dir tests/integration_tests/templates/api/json &&
-python run_ui_tests.py &&
 echo done ||
 echo failed
+
+kill $API_PID
