3034622f7a2abe2264011e1dd4e0a6e45ec97f02
Richard Challis <richard.challis@sanger.ac.uk>
2023-08-14 10:33:43 +0100

    show failed doc in logs

--- src/genomehubs/lib/es_functions.py
+++ src/genomehubs/lib/es_functions.py
@@ -163,6 +163,25 @@ def load_mapping(es, mapping_name, mapping):
     return res
 
 
+def get_size(obj, seen=None):
+    """Recursively find size of objects."""
+    size = sys.getsizeof(obj)
+    if seen is None:
+        seen = set()
+    obj_id = id(obj)
+    if obj_id in seen:
+        return 0
+    seen.add(obj_id)
+    if isinstance(obj, dict):
+        size += sum([get_size(v, seen) for v in obj.values()])
+        size += sum([get_size(k, seen) for k in obj.keys()])
+    elif hasattr(obj, "__dict__"):
+        size += get_size(obj.__dict__, seen)
+    elif hasattr(obj, "__iter__") and not isinstance(obj, (str, bytes, bytearray)):
+        size += sum([get_size(i, seen) for i in obj])
+    return size
+
+
 def index_stream(
     es,
     index_name,
@@ -234,6 +253,11 @@ def index_stream(
                 else:
                     es.update(index=index_name, id=action["_id"], doc=action["doc"])
             except Exception as err:
+                LOGGER.warn(
+                    "Size of document that failed to index is %d bytes",
+                    get_size(action),
+                )
+                LOGGER.warn(action)
                 raise err
         raise bulk_err
     es_client = client.IndicesClient(es)
