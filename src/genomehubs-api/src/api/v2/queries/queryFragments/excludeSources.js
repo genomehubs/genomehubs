4b34253d783d52c201fee5ea34922fba770ff509
Richard Challis <richard.challis@sanger.ac.uk>
2023-10-06 10:45:06 +0100

    fix nested queries when fields mismatch

--- src/genomehubs-api/src/api/v2/queries/queryFragments/excludeSources.js
+++ src/genomehubs-api/src/api/v2/queries/queryFragments/excludeSources.js
@@ -23,88 +23,71 @@ export const excludeSources = (exclusions = {}, fields, result) => {
     ];
   };
   let excluded = [];
-  if (result == "taxon" || result == "assembly" || result == "sample") {
-    Object.keys(exclusions).forEach((source) => {
-      if (source == "unclassified") {
-        excluded.push({
-          nested: {
-            path: "lineage",
-            query: {
-              prefix: { "lineage.scientific_name": { value: "unclassified" } },
-            },
+  Object.keys(exclusions).forEach((source) => {
+    if (source == "unclassified") {
+      excluded.push({
+        nested: {
+          path: "lineage",
+          query: {
+            prefix: { "lineage.scientific_name": { value: "unclassified" } },
           },
-        });
-        excluded.push({
-          nested: {
-            path: "lineage",
-            query: {
-              prefix: { "lineage.scientific_name": { value: "environmental" } },
-            },
+        },
+      });
+      excluded.push({
+        nested: {
+          path: "lineage",
+          query: {
+            prefix: { "lineage.scientific_name": { value: "environmental" } },
           },
-        });
+        },
+      });
 
-        return;
-      }
+      return;
+    }
 
-      exclusions[source]
-        .filter((field) => typeof field !== "undefined")
-        .forEach((field) => {
-          if (source == "missing") {
-            try {
-              delete fields[field];
-            } catch (err) {
-              // needed in case field is an array method (e.g. length)
-            }
-            excluded.push({
-              bool: {
-                must_not: {
-                  nested: {
-                    path: "attributes",
-                    query: {
-                      match: { "attributes.key": field },
-                    },
-                  },
-                },
-              },
-            });
-            return;
+    exclusions[source]
+      .filter((field) => typeof field !== "undefined")
+      .forEach((field) => {
+        if (source == "missing") {
+          try {
+            delete fields[field];
+          } catch (err) {
+            // needed in case field is an array method (e.g. length)
           }
           excluded.push({
-            nested: {
-              path: "attributes",
-              query: {
-                bool: {
-                  filter: [
-                    {
-                      match: { "attributes.key": field },
-                    },
-                    {
-                      match: {
-                        "attributes.aggregation_source": source,
-                      },
-                    },
-                  ].concat(preserveMultiple({ field, source })),
+            bool: {
+              must_not: {
+                nested: {
+                  path: "attributes",
+                  query: {
+                    match: { "attributes.key": field },
+                  },
                 },
               },
             },
           });
+          return;
+        }
+        excluded.push({
+          nested: {
+            path: "attributes",
+            query: {
+              bool: {
+                filter: [
+                  {
+                    match: { "attributes.key": field },
+                  },
+                  {
+                    match: {
+                      "attributes.aggregation_source": source,
+                    },
+                  },
+                ].concat(preserveMultiple({ field, source })),
+              },
+            },
+          },
         });
-    });
-  } else {
-    Object.keys(exclusions).forEach((source) => {
-      exclusions[source]
-        .filter((field) => typeof field !== "undefined")
-        .forEach((field) => {
-          if (source == "missing") {
-            try {
-              delete fields[field];
-            } catch (err) {
-              // needed in case field is an array method (e.g. length)
-            }
-            return;
-          }
-        });
-    });
-  }
+      });
+  });
   return excluded;
 };
