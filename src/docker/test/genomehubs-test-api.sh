ada18821fd756c7a9510e3a248b59e2cea54d4e3
Richard Challis <richard.challis@sanger.ac.uk>
2023-10-31 13:17:55 +0000

    update scripts

--- src/docker/test/genomehubs-test-api.sh
+++ src/docker/test/genomehubs-test-api.sh
@@ -2,21 +2,27 @@
 
 cp /genomehubs/config/config.yaml /tmp/config.yaml
 
-if [ ! -z "$GH_RELEASE" ]; then
-  cat /tmp/config.yaml | yq -y '.test.hub.version="'$GH_RELEASE'"' > TMP && mv TMP /tmp/config.yaml
+if [ -z "$GH_RELEASE" ]; then
+  export GH_RELEASE=2021.10.15
 fi
 
-if [ ! -z "$GH_HUBNAME" ]; then
-  cat /tmp/config.yaml | yq -y '.test.hub.name="'$GH_HUBNAME'"' > TMP && mv TMP /tmp/config.yaml
+if [ -z "$GH_HUBNAME" ]; then
+  export GH_HUBNAME="genomehubs"
 fi
 
-# if [ -z "$GH_NODE" ]; then
-#   cat /tmp/config.yaml | yq -y '.test.es.host=["'$GH_NODE'"]' > TMP && mv TMP /tmp/config.yaml
-# fi
+if [ -z "$GH_NODE" ]; then
+  export GH_NODE="http://localhost:9200"
+fi
 
-cat /tmp/config.yaml
+if [ -z "$GH_RESOURCES" ]; then
+  export GH_RESOURCES="/genomehubs/resources"
+fi
+
+cat /tmp/config.yaml | yq -y '.test.hub.version="'$GH_RELEASE'"' > TMP && mv TMP /tmp/config.yaml
+cat /tmp/config.yaml | yq -y '.test.hub.name="'$GH_HUBNAME'"' > TMP && mv TMP /tmp/config.yaml
 
 /genomehubs/genomehubs-api &
+API_PID=$!
 
 sleep 5
 
@@ -24,3 +30,5 @@ genomehubs test \
     --config-file /tmp/config.yaml \
     --base-url http://localhost:3000/api/v2 \
     --json-test-dir /genomehubs/tests
+
+kill $API_PID
--- src/docker/test/genomehubs-test-ui.sh
+++ src/docker/test/genomehubs-test-ui.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
-/genomehubs/genomehubs-api &
-/genomehubs/genomehubs-ui &
+if [ -z "$GH_RELEASE" ]; then
+  export GH_RELEASE=2021.10.15
+fi
 
+if [ -z "$GH_HUBNAME" ]; then
+  export GH_HUBNAME="genomehubs"
+fi
+
+if [ -z "$GH_NODE" ]; then
+  export GH_NODE="http://localhost:9200"
+fi
+
+if [ -z "$GH_RESOURCES" ]; then
+  export GH_RESOURCES="/genomehubs/resources"
+fi
+
+/genomehubs/genomehubs-api &
+API_PID=$!
+/genomehubs/genomehubs-ui &
+UI_PID=$!
 sleep 5
 
 node /genomehubs/test-ui.mjs /genomehubs/tests /genomehubs/tests-out
+
+kill $API_PID
+kill $UI_PID
