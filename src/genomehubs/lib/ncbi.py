270e03e0815069246f93c64649fe10c0935e40b0
Richard Challis <richard.challis@sanger.ac.uk>
2023-11-09 08:39:30 +0000

    add sra xml parsing

--- src/genomehubs/lib/ncbi.py
+++ src/genomehubs/lib/ncbi.py
@@ -2,10 +2,17 @@
 """NCBI functions."""
 
 import contextlib
+import csv
 import gzip
+import os
 import re
 from collections import Counter
+from collections import defaultdict
+from itertools import groupby
+from operator import itemgetter
 from urllib.error import ContentTooShortError
+
+import xml.etree.ElementTree as ET
 
 import ujson
 from Bio import SeqIO
@@ -384,3 +391,162 @@ def ncbi_genome_parser(params, opts, *, types=None, names=None):
             else:
                 parse_ncbi_datasets_record(record, parsed)
     return list(parsed.values())
+
+
+def split_chunks(values, split_val):
+    """Split a list of values into chunks at a certain value."""
+    index = 0
+
+    def chunk_index(val):
+        nonlocal index
+        if val == split_val:
+            index += 1
+        return index
+
+    return groupby(values, chunk_index)
+
+
+def read_exp_xml(node, obj):
+    """Read values from ExpXml section."""
+    for child in node:
+        tag = child.tag
+        if tag == "Bioproject":
+            obj["bioproject"] = child.text
+        elif tag == "Biosample":
+            obj["biosample"] = child.text
+        elif tag == "Organism":
+            obj["taxon_id"] = child.get("taxid")
+        elif tag == "Experiment":
+            obj["sra_accession"] = child.get("acc")
+        elif tag == "Summary":
+            obj["platform"] = child.findtext("Platform")
+        elif tag == "Library_descriptor":
+            obj["library_source"] = child.findtext("LIBRARY_SOURCE").lower()
+
+
+def read_runs(node, obj):
+    """Read values from Runs section."""
+    if "runs" not in obj:
+        obj["runs"] = []
+    for child in node:
+        obj["runs"].append(
+            {"accession": child.get("acc"), "reads": child.get("total_spots")}
+        )
+
+
+def parse_sra_xml(xml_file):
+    """Parse an SRA xml file."""
+    rows = []
+    with open(xml_file, "r", encoding="utf8") as container_file:
+        for doc_num, doc in split_chunks(
+            container_file, '<?xml version="1.0" encoding="UTF-8" ?>\n'
+        ):
+            print(f"processing sub-document #{doc_num}")
+            print(len(rows))
+            lines = list(doc)
+            try:
+                root = ET.fromstringlist(lines)
+            except Exception:
+                continue
+            for doc_summary in root.iter("DocumentSummary"):
+                obj = {}
+                for child in doc_summary:
+                    tag = child.tag
+                    if tag == "CreateDate":
+                        obj["date"] = child.text
+                    elif tag == "ExpXml":
+                        read_exp_xml(child, obj)
+                    elif tag == "Runs":
+                        read_runs(child, obj)
+                    continue
+                rows.append(obj)
+    return rows
+
+
+def group_by_taxon(rows, grouped=None):
+    """
+    Group SRA runs by taxon.
+
+    Keep the most recent 10 rows only.
+    """
+    if grouped is None:
+        grouped = defaultdict(lambda: {"count": 0, "reads": 0, "runs": []})
+    for obj in sorted(rows, key=itemgetter("date")):
+        try:
+            taxon_id = obj["taxon_id"]
+        except Exception:
+            continue
+        for run in obj["runs"]:
+            try:
+                row = {
+                    **obj,
+                    "run_accession": run["accession"],
+                    "reads": int(run["reads"]),
+                }
+            except Exception:
+                continue
+            row.pop("runs")
+            grouped[taxon_id]["runs"].insert(0, row)
+            grouped[taxon_id]["count"] += 1
+            grouped[taxon_id]["reads"] += row["reads"]
+            if len(grouped[taxon_id]["runs"]) > 10:
+                grouped[taxon_id]["runs"].pop()
+    rows = [
+        {
+            "taxon_id": taxon_id,
+            "sra_accession": ";".join([item["sra_accession"] for item in obj["runs"]]),
+            "run_accession": ";".join([item["run_accession"] for item in obj["runs"]]),
+            "library_source": ";".join(
+                [item["library_source"] for item in obj["runs"]]
+            ),
+            "platform": ";".join([item["platform"] for item in obj["runs"]]),
+            "reads": ";".join([str(item["reads"]) for item in obj["runs"]]),
+            "total_reads": obj["reads"],
+            "total_runs": obj["count"],
+        }
+        for taxon_id, obj in grouped.items()
+    ]
+    return rows
+
+
+def load_tsv(file):
+    """Load rows from a TSV file."""
+    grouped = defaultdict(lambda: {"count": 0, "reads": 0, "runs": []})
+    with open(file, newline="") as tsv_file:
+        reader = csv.DictReader(tsv_file, delimiter="\t")
+        for row in reader:
+            taxon_id = row["taxon_id"]
+            total_runs = row["total_runs"]
+            total_reads = row["total_reads"]
+            grouped[taxon_id]["count"] = int(total_runs)
+            grouped[taxon_id]["reads"] = int(total_reads)
+            for key in (
+                "run_accession",
+                "sra_accession",
+                "library_source",
+                "platform",
+                "reads",
+            ):
+                row[key] = row[key].split(";")
+            for index, run_accession in enumerate(row["run_accession"]):
+                grouped[taxon_id]["runs"].append(
+                    {
+                        "run_accession": run_accession,
+                        "sra_accession": row["sra_accession"][index],
+                        "library_source": row["library_source"][index],
+                        "platform": row["platform"][index],
+                        "reads": int(row["reads"][index]),
+                    }
+                )
+    return grouped
+
+
+def sra_parser(params, opts, *, types=None, names=None):
+    """Parse SRA efetch xml."""
+    outfile = opts["outfile"]
+    # Load older runs from tsv
+    previous = load_tsv(outfile) if os.path.isfile(outfile) else None
+    xml_file = opts["sra"]
+    # Load newer runs from xml
+    rows = parse_sra_xml(xml_file)
+    return group_by_taxon(rows, grouped=previous)
