961cc214f4d13e4aaf9727bc850dc186bb337746
Richard Challis <richard.challis@sanger.ac.uk>
2024-05-30 11:23:21 +0100

    "Bump version: 2.8.16 → 2.8.17"

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ try:
     version = release = get_distribution("genomehubs").version
 except Exception:
     traceback.print_exc()
-    version = release = "2.8.16"
+    version = release = "2.8.17"
 
 pygments_style = "trac"
 templates_path = ["."]
--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*names, **kwargs):
 
 setup(
     name="genomehubs",  # Required
-    version="2.8.16",
+    version="2.8.17",
     description="GenomeHubs",  # Optional
     long_description="%s\n%s"
     % (
