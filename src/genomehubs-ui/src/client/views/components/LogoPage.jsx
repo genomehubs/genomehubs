240fe0bb444a27e7a4a824534e21aee1cc334553
Richard Challis <richard.challis@sanger.ac.uk>
2025-02-13 10:28:31 +0000

    add LogoPage component and integrate into Main routing

---
+++ src/genomehubs-ui/src/client/views/components/LogoPage.jsx
@@ -0,0 +1,35 @@
+import Logo from "./Logo";
+import React from "react";
+
+export const LogoPage = () => {
+  return (
+    <div
+      style={{
+        height: "calc( 100vh - 8em )",
+        width: "100vw",
+        maxWidth: "100%",
+        maxHeight: "100%",
+        backgroundColor: "rgb(49, 50, 63)",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          height: "calc( 90vh - 8em )",
+          width: "calc( 90vh - 8em )",
+          maxWidth: "90%",
+          maxHeight: "90%",
+          backgroundColor: "rgb(49, 50, 63)",
+          border: "1em solid rgba(255,255,255, 0.2)",
+          boxSizing: "border-box",
+        }}
+      >
+        <Logo />
+      </div>
+    </div>
+  );
+};
+
+export default LogoPage;
