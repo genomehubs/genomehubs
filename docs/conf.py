6c081313818deb30563499ad226dcc1324e3816d
Richard Challis <richard.challis@sanger.ac.uk>
2025-03-14 17:10:21 +0000

    "Bump version: 2.10.9 → 2.10.10"

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ try:
     version = release = get_distribution("genomehubs").version
 except Exception:
     traceback.print_exc()
-    version = release = "2.10.9"
+    version = release = "2.10.10"
 
 pygments_style = "trac"
 templates_path = ["."]
--- scripts/conda_build.sh
+++ scripts/conda_build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION=2.10.9
+VERSION=2.10.10
 
 case $(uname | tr '[:upper:]' '[:lower:]') in
   linux*)
