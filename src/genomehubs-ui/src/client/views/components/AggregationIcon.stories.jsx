4e8ea9ea1598fdea1059bdff5843cfd3f778158f
Chandrika Singh Jadon <jadonchandrika@gmail.com>
2025-03-18 00:02:08 +0530

    changes

--- src/genomehubs-ui/src/client/views/components/AggregationIcon.stories.jsx
+++ src/genomehubs-ui/src/client/views/components/AggregationIcon.stories.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import AggregationIcon from "../components/AggregationIcon";
 import { Provider } from "react-redux";
-import store from "../reducers/color.store"; // Update this based on actual store path
+import store from "../reducers/color.store"; 
 
 export default {
   title: "Components/AggregationIcon",
@@ -21,9 +21,6 @@ export default {
     hasDescendants: {
       control: { type: "boolean" },
     },
-    statusColors: {
-      control: "object",
-    },
   },
 };
 
@@ -33,19 +30,22 @@ export const Direct = Template.bind({});
 Direct.args = {
   method: "direct",
   hasDescendants: false,
-  statusColors: { direct: "green", descendant: "orange", ancestor: "red" },
+};
+
+export const DirectWithDescendants = Template.bind({});
+DirectWithDescendants.args = {
+  method: "direct",
+  hasDescendants: true,
 };
 
 export const Descendant = Template.bind({});
 Descendant.args = {
   method: "descendant",
-  hasDescendants: true,
-  statusColors: { direct: "green", descendant: "orange", ancestor: "red" },
+  hasDescendants: false,
 };
 
 export const Ancestor = Template.bind({});
 Ancestor.args = {
   method: "ancestor",
   hasDescendants: false,
-  statusColors: { direct: "green", descendant: "orange", ancestor: "red" },
 };
