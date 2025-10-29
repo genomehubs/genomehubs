d9a11fc4590b9bba45e4a064df0201fa67dd398a
Richard Challis <richard.challis@sanger.ac.uk>
2025-10-29 15:22:56 +0000

    "Bump version: 2.11.13 → 2.11.14"

--- scripts/conda_build.sh
+++ scripts/conda_build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION=2.11.13
+VERSION=2.11.14
 
 case $(uname | tr '[:upper:]' '[:lower:]') in
   linux*)
--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*names, **kwargs):
 
 setup(
     name="genomehubs",  # Required
-    version="2.11.13",
+    version="2.11.14",
     description="GenomeHubs",  # Optional
     long_description="%s\n%s"
     % (
