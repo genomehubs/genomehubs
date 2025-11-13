141eb1aeaf8a84c504151efb81e85127187fbbc5
Richard Challis <richard.challis@sanger.ac.uk>
2025-11-13 15:08:00 +0000

    "Bump version: 2.11.16 → 2.11.17"

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ try:
     version = release = get_version("genomehubs")
 except Exception:
     traceback.print_exc()
-    version = release = "2.11.16"
+    version = release = "2.11.17"
 
 pygments_style = "trac"
 templates_path = ["."]
--- scripts/conda_build.sh
+++ scripts/conda_build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-VERSION=2.11.16
+VERSION=2.11.17
 
 case $(uname | tr '[:upper:]' '[:lower:]') in
   linux*)
