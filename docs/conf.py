0e09d0d0f1e0f01a3a2f1b7b9610d78c487f6579
Richard Challis <richard.challis@sanger.ac.uk>
2026-02-19 16:41:58 +0000

    "Bump version: 2.12.2 → 2.12.3"

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ try:
     version = release = get_version("genomehubs")
 except Exception:
     traceback.print_exc()
-    version = release = "2.12.2"
+    version = release = "2.12.3"
 
 pygments_style = "trac"
 templates_path = ["."]
