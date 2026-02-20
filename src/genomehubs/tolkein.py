c52d5de1994bf749a884afd855d0e67f3d2a7046
Richard Challis <richard.challis@sanger.ac.uk>
2026-02-20 10:33:31 +0000

    add stable re-exports for vendored tolkein function

---
+++ src/genomehubs/tolkein.py
@@ -0,0 +1,7 @@
+"""Stable public re-exports for selected tolkein helpers."""
+
+from .vendor.tolkein import tofetch
+from .vendor.tolkein import tofile
+from .vendor.tolkein import tolog
+
+__all__ = ["tofetch", "tofile", "tolog"]
